58f7ce99b8ee72f29c90a1cf140020d352f3b37e
Rafael Carrilho <carrilhorafael57@gmail.com>
2022-06-09 21:04:25 -0700

    edit function complete

--- resources/views/categories/edit.blade.php
+++ resources/views/categories/edit.blade.php
@@ -12,6 +12,8 @@
                   <form method="POST" action=" {{ route('categories.update', $category ) }} ">
 
                     @method('PUT')
+                    {{-- specific token for form --}}
+                    @csrf
                     Name: <input name="name" type="text" value=" {{ $category->name }} " class="rounded-md shadow-sm border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"/>
                     <br />
                     <br />
